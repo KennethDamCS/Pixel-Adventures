1ef4745d23eeb150e0db6bcf8633c53717970d36
KennethDamCS <mrdam.kiet@gmail.com>
2023-12-28 17:38:44 -0800

    Implemented player death animation and logic

---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    private Animator animator;
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            Die();
+        }
+    }
+
+
+    private void Die()
+    {   
+        rigidBody.bodyType = RigidbodyType2D.Static;
+        animator.SetTrigger("death");
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
